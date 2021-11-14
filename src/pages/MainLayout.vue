679b1bfe1f48824b71aaa98bb6963825856e8389
haidi <haidinurhadinata@gmail.com>
2021-11-14 15:14:38 +0800

    fix view navbar on MainLayout

--- src/pages/MainLayout.vue
+++ src/pages/MainLayout.vue
@@ -21,7 +21,7 @@
               <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
             </svg>
             <p
-              class=" text-white font-bold text-2xl ml-16 "
+              class=" text-white font-bold text-2xl ml-20 "
               style="display:inline" >
               SayapKiri
             </p>
