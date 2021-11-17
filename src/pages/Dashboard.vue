d635e690a2d941555356f3c738957e7e0c1532a7
haidi <haidinurhadinata@gmail.com>
2021-11-17 21:39:32 +0800

    add hidden name on Dashboard

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -52,7 +52,7 @@
                 </div>
                 <div class="text-green-600">
                   <!-- {{item.account_name}} -->
-                  {{ sortName(item.account_name) }}
+                  {{ !hiddenName ? sortName(item.account_name) : "*****" }}
                 </div>
               </div>
             </div>
@@ -160,6 +160,30 @@
     </div>
     <button
       type="button"
+      @click="hiddenName = !hiddenName"
+      class="
+        bg-white
+        border-4 border-green-300
+        text-white
+        font-bold
+        py-2
+        px-2
+        rounded-lg
+        focus:outline-none focus:shadow-outline
+        btn-hidden-name
+      "
+    >
+        <svg v-if="!hiddenName" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-eye text-green-300 " width="20" viewBox="0 0 16 16">
+            <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
+            <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
+        </svg>
+        <svg v-if="hiddenName" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-eye-slash-fill text-green-300 " width="20" viewBox="0 0 16 16">
+            <path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7.029 7.029 0 0 0 2.79-.588zM5.21 3.088A7.028 7.028 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474L5.21 3.089z"/>
+            <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829l-2.83-2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12-.708.708z"/>
+        </svg>
+    </button>
+    <button
+      type="button"
       @click="toggleModal"
       class="
         bg-white
@@ -211,6 +235,12 @@
 </template>
 
 <style lang="scss">
+.btn-hidden-name {
+    position: fixed;
+    right: 15px;
+    bottom: 120px;
+}
+
 .btn-orderBy {
   position: fixed;
   right: 15px;
@@ -222,6 +252,8 @@
   right: 15px;
   bottom: 30px;
 }
+
+
 </style>
 
 <script>
@@ -236,6 +268,7 @@ export default {
   data() {
     return {
       loading: false,
+      hiddenName: false,
       token: localStorage.getItem("token"),
       list: [],
       request: {
@@ -309,13 +342,27 @@ export default {
       let elementCard = document.getElementById("card_" + index);
 
       html2canvas(elementCard).then((canvas) => {
-        try{
-            canvas.toBlob((blob) =>
-                navigator.clipboard.write([new ClipboardItem({ "image/png": blob })])
-            )
-        }catch(err) {
-            console.error(err.name, err.message);
+        try {
+          canvas.toBlob((blob) =>
+            navigator.clipboard.write([
+              new ClipboardItem({ "image/png": blob }),
+            ])
+          );
+        } catch (err) {
+          console.error(err.name, err.message);
         }
+
+        this.$swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timerProgressBar: true,
+            timer: 2000,
+        })
+        .fire({
+            icon: "success",
+            title: "Copy image",
+        });
       });
     },
   },
