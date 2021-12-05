fa8d724639eab50ee18dbd9ee5fded731fe89364
haidi <haidinurhadinata@gmail.com>
2021-12-05 11:17:54 +0800

    add button filter on Transaction RN

---
+++ src/pages/LastData.js
@@ -0,0 +1,71 @@
+import axios from "axios";
+import MainLayout from "@/pages/MainLayout";
+
+export default {
+    data() {
+        return {
+            token: localStorage.getItem("token"),
+            table: [],
+            loading: false,
+            indexSelected: null,
+            request: {
+                field: "dd",
+                sorted: "desc",
+            },
+        };
+    },
+    components: {
+        MainLayout,
+    },
+    mounted() {
+        this.getLastData();
+    },
+    methods: {
+        async getLastData() {
+            let that = this;
+            this.table = [];
+            this.loading = true;
+
+            await axios
+                .post(
+                    process.env.VUE_APP_BASE_URL + "api/dashboard/last-data",
+                    this.request, { headers: { Authorization: `Bearer ` + that.token } }
+                )
+                .then((responses) => {
+                    let status = responses.data.status;
+                    let data = responses.data.data;
+
+                    if (status) {
+                        this.table = data;
+                    }
+
+                    this.loading = false;
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        },
+        customEquity(number) {
+            if (number !== null || number !== undefined) {
+                return Intl.NumberFormat("de-DE", {
+                    style: "currency",
+                    currency: "USC",
+                }).format(number);
+            }
+        },
+        customNumber(number) {
+            // return Intl.NumberFormat(2).format(number);
+            if (number !== null || number !== undefined) {
+                return number.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+            }
+        },
+        activeRow(index) {
+            this.indexSelected = index;
+        },
+        classActiveRow(index) {
+            return {
+                active: this.indexSelected == index ? true : false,
+            };
+        },
+    },
+};
--- src/pages/TransactionRn.vue
+++ src/pages/TransactionRn.vue
@@ -3,10 +3,35 @@
         <div class="grid md:mt-28 mt-10 md:mx-6">
             <!-- start table desktop -->
             <div class="bg-white shadow-md rounded pt-4 hidden-mobile px-2">
-                <p class="text-3xl mb-5">Last Data</p>
-                <!-- <vue-good-table
-                    :columns="columns"
-                    :rows="rows"/> -->
+                <p class="text-3xl mb-5">Transaction RN</p>
+                <div class="flex flex-row">
+                    <div class="mb-4">
+                        <label class="label-custom">Name</label>
+                        <input class=" input-custom " type="text" v-model="request.user" >
+                    </div>
+                    <div class=" ml-2 ">
+                        <label class="label-custom">Account</label>
+                        <input class=" input-custom " type="text" v-model="request.account" >
+                    </div>
+                    <div class=" ml-2 ">
+                        <label class="label-custom">Tgl</label>
+                        <input class=" input-custom " type="date" v-model="request.start_date"  >
+                    </div>
+                    <div class="">
+                        <div class="mb-7" ></div>
+                        <input class=" ml-2 input-custom " type="date" v-model="request.stop_date"  >
+                    </div>
+                    <div>
+                        <div class="mb-7" ></div>
+                        <button 
+                            type="submit" 
+                            class=" ml-8 bg-green-500 text-white rounded-md p-2 "
+                            @click="getTransactionRn">
+                            Apply
+                        </button>
+                    </div>
+                </div>
+                
                 <vue-good-table  
                     :rows="rows"
                     :columns="columns"
@@ -23,6 +48,7 @@
 
 <script>
 import axios from "axios";
+import moment from 'moment';
 import MainLayout from "@/pages/MainLayout";
 import { VueGoodTable } from 'vue-good-table-next';
 
@@ -31,6 +57,12 @@ export default {
         return {
             token: localStorage.getItem("token"),
             rows: [],
+            request: {
+                user: null,
+                account: null,
+                stop_date: moment().format('YYYY-MM-DD'),
+                start_date: moment().startOf('month').format('YYYY-MM-DD'),
+            },
             paginationOptions: {
                 perPage: 15,
                 enabled: true,
