07e8ecee4987af6ce9ed74042cfc299e05207d7e
haidi <haidinurhadinata@gmail.com>
2021-12-19 09:01:39 +0800

    add loading on Dashboard

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -15,6 +15,23 @@
         <p class="">Data Empty</p>
       </div>
     </div>
+    <div v-if="list.length <= 0 && loading" class="mt-28 mx-6 ">
+      <div v-for="index in 12" :key="index" class="border border-gray-300 shadow rounded-md p-4 max-w-sm w-full inline-block m-4 ">
+        <div class="animate-pulse flex space-x-4">
+          <div class="flex-1 space-y-6 py-1">
+            <div v-for="index in 3" :key="index" class="space-y-3">
+              <div class="grid grid-cols-3 gap-4">
+                <div class="h-2 bg-white rounded col-span-2"></div>
+                <div class="h-2 bg-white rounded col-span-1"></div>
+              </div>
+              <div class="h-2 bg-white rounded"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <!-- main card -->
     <div
       v-if="list.length > 0"
       class="grid md:grid-cols-4 md:gap-5 md:mx-10 md:my-6 mt-12 md:mt-20"
