95d9ae1130e28ae33496661ac9e10f4e1a191acb
haidi <haidinurhadinata@gmail.com>
2022-03-07 09:13:45 +0800

    memperbaiki action

--- src/pages/UserController.js
+++ src/pages/UserController.js
@@ -133,7 +133,7 @@ export default {
         },
         async onRemove(pid_user) {
             let that = this;
-            let foundData = this.table.data.filter(item => item.pid_user == pid_user)[0];
+            let foundData = this.dataRows.filter(item => item.pid_user == pid_user)[0];
 
             return await this.$swal.fire({
                 icon: 'question',
@@ -187,16 +187,15 @@ export default {
                 }
             })
         },
-        onEdit(params) {
-            console.log(params);
+        onEdit(pid_user) {
             // let that = this;        
-            // let foundData = this.table.data.filter(item => item.pid_user == pid_user)[0];
+            let foundData = this.dataRows.filter(item => item.pid_user == pid_user)[0];
 
-            // this.form.pid_user = foundData.pid_user;
-            // this.form.nama = foundData.nama;
-            // this.form.email = foundData.email;
-            // this.form.no_telp = foundData.no_telp;
-            // this.form.register_date = moment(foundData.register_date).format('YYYY-MM-DD');
+            this.form.pid_user = foundData.pid_user;
+            this.form.nama = foundData.nama;
+            this.form.email = foundData.email;
+            this.form.no_telp = foundData.no_telp;
+            this.form.register_date = moment(foundData.register_date).format('YYYY-MM-DD');
         },
         resetForm() {
             this.form = initialState();
