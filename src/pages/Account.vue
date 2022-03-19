b94354d7842b439aae2855a0f27a2473731e0ad3
haidi <haidinurhadinata@gmail.com>
2022-03-19 10:10:39 +0800

    menambahkan pilihan rebate pada account page

--- src/pages/Account.vue
+++ src/pages/Account.vue
@@ -88,6 +88,7 @@
                                 class="block appearance-none w-full bg-white border py-2 px-3 rounded shadow-sm border-gray-300 leading-tight focus:outline-none focus:shadow-outline">
                                 <option value="active">Active</option>
                                 <option value="inactive">In Active</option>
+                                <option value="rebate">Rebate</option>
                             </select>
                             <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
